cb7502d290c0f03379bf2aa81d0bbc407a7052c2
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-07-08 13:01:44 +0200

    Add a simple performance test

---
+++ example/performance.js
@@ -0,0 +1,27 @@
+const { init, subscribe } = require('state-prism')
+const { performance } = require('perf_hooks')
+
+const state = init({
+  foo: 0,
+})
+
+const measurePerformance = () => {
+  const before = performance.now()
+
+  let unsubscribes = []
+  for (let index = 0; index < 200; index++) {
+    const unsubscribe = subscribe('foo', () => {})
+    unsubscribes.push(unsubscribe)
+  }
+  state.foo += 1
+
+  unsubscribes.forEach((unsubscribe) => {
+    unsubscribe()
+  })
+
+  const after = performance.now()
+  const delta = after - before
+  console.log('Time to run:', delta)
+}
+
+measurePerformance()
